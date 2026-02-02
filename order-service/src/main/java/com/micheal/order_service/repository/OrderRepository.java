afd14f96c4edd2b99070d3be78027509ec2dd870
anhphan237 <anh713988@gmail.com>
2026-02-02 22:06:38 +0700

    add inventory-sáºáºervice

---
+++ order-service/src/main/java/com/micheal/order_service/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.micheal.order_service.repository;
+
+import com.micheal.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
